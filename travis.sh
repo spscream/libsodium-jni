fb6322087f9a51829955bbaa00c6095d52e97d1a
joshjdevl <joshjdevl@gmail.com>
2017-02-27 08:25:03 -0800

    osx build

--- travis.sh
+++ travis.sh
@@ -2,7 +2,9 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     #true #comment out for now
-    gradle build --full-stacktrace
+    #gradle build --full-stacktrace
+    ./build.sh
+    ./build-hostjni.sh
 else
     #true # Building for Linux is now fully implemented by "gradle build". 
     gradle build --full-stacktrace
