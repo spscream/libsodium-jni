0a82b57c84a65eb8be01f9d1db0c2258d5f26856
joshjdevl <joshjdevl@gmail.com>
2017-02-26 21:44:43 -0800

    clean up env vars

--- setenv.sh
+++ setenv.sh
@@ -2,6 +2,8 @@
 
 export GRADLE_OPTS="-Dorg.gradle.native=false -Dorg.gradle.daemon=true"
 export NDK_VERSION=r13b
+export NDK_TOOLCHAIN_PLATFORM=14
+export NDK_TOOLCHAIN_ARCHITECTURE=arm
 export ANDROID_SDK_VERSION=r24.4.1
 
 if uname -a | grep -q -i darwin; then
@@ -13,16 +15,14 @@ if uname -a | grep -q -i darwin; then
 else
     export MAVEN_VERSION=3.3.9
     export GRADLE_VERSION=3.3
-    export NDK_TOOLCHAIN_PLATFORM=14
-    export NDK_TOOLCHAIN_ARCHITECTURE=arm
-    export NDK_ROOT=`pwd`/installs/android-ndk-${NDK_VERSION}
-    export PATH=${NDK_ROOT}:$PATH
+    export ANDROID_NDK_HOME=`pwd`/installs/android-ndk-${NDK_VERSION}
     export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-    export ANDROID_NDK_HOME=${NDK_ROOT}
     export ANDROID_HOME=`pwd`/installs/android-sdk-linux
     export ANDROID_SDK=`pwd`/installs/android-sdk-linux
-    export PATH=`pwd`/installs/apache-maven-${MAVEN_VERSION}:`pwd`/installs/gradle-${GRADLE_VERSION}/bin:`pwd`/installs/android-sdk-linux/tools:`pwd`/installs/android-toolchain/bin:$PATH
+    export PATH=`pwd`/installs/apache-maven-${MAVEN_VERSION}:`pwd`/installs/gradle-${GRADLE_VERSION}/bin:`pwd`/installs/android-sdk-linux/tools:$PATH
 fi
 
 export PATH=$ANDROID_SDK/tools:$ANDROID_SDK/tools/bin:$ANDROID_SDK/platform-tools:${ANDROID_NDK_HOME}:$PATH
+
+export PATH=`pwd`/installs/android-toolchain/bin:$PATH
 
