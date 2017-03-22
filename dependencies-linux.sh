e19a52e7c606b1daa5850a703b6f694f54afa442
Alexander Malaev <am@malaev.net>
2017-03-23 01:20:17 +0300

    Add unified headers

--- dependencies-linux.sh
+++ dependencies-linux.sh
@@ -17,8 +17,8 @@ sudo apt-fast -qq update
 sudo apt-fast -y -qq install wget git autoconf autoconf automake build-essential autogen libtool gettext-base gettext vim bzip2 libpcre3-dev libpcre++-dev pkg-config
 #gradle building issues
 sudo apt-fast -y -qq install gcc-multilib lib32z1
-sudo add-apt-repository ppa:webupd8team/java -y 
-sudo apt-fast -qq update 
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-fast -qq update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-fast -y -qq install oracle-java8-installer maven
 
@@ -27,7 +27,7 @@ pushd ./installs
 wget --quiet http://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip
 chmod 755 android-ndk-${NDK_VERSION}-linux-x86_64.zip
 test -e "android-ndk-${NDK_VERSION}" || unzip -qq "android-ndk-${NDK_VERSION}"-linux-x86_64.zip
-test -e `pwd`/installs/android-toolchain || ${NDK_ROOT}/build/tools/make_standalone_toolchain.py -v --api=${NDK_TOOLCHAIN_PLATFORM} --arch=${NDK_TOOLCHAIN_ARCHITECTURE} --install-dir=`pwd`/installs/android-toolchain 
+test -e `pwd`/installs/android-toolchain || ${NDK_ROOT}/build/tools/make_standalone_toolchain.py -v --unified-headers --api=${NDK_TOOLCHAIN_PLATFORM} --arch=${NDK_TOOLCHAIN_ARCHITECTURE} --install-dir=`pwd`/installs/android-toolchain
 popd
 
 pushd ./installs
@@ -39,6 +39,6 @@ pushd jni
 ./installswig.sh
 popd
 
-./download-gradle.sh 
+./download-gradle.sh
 
 ./update-android.sh
